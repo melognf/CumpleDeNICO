f79610409ea7ae286bb66cea8ca9c1c277299eb2
melognf <melognf@yahoo.com.ar>
2024-08-06 18:37:54 -0300

    nuevo cumple

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
     // This is an example with default parameters
 
     simplyCountdown('#cuenta', {
-        year: 2023, // required
+        year: 2024, // required
         month: 8, // required
-        day: 27, // required
-        hours: 0, // Default is 0 [0-23] integer
+        day: 25, // required
+        hours: 15, // Default is 0 [0-23] integer
         minutes: 0, // Default is 0 [0-59] integer
         seconds: 0, // Default is 0 [0-59] integer
         words: { //words displayed into the countdown
